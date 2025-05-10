30cbc90d558e9bd83260955425f3ebb5cab8b077
AnnishaSiddika <annisha2112@gmail.com>
2025-05-10 22:57:57 +0100

    add client

---
+++ client/src/app/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from 'react';
+
+export default function HomePage() {
+  
+
+  return (
+    <main className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Users</h1>
+      
+    </main>
+  );
+}
