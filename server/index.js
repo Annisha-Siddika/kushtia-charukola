6df5a90345e89c26ec7db514bd367cd0e73cf379
raihanuldev <rihanthedev@gmail.com>
2025-05-12 22:19:54 +0600

    User Authentication API-done

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const userRoutes = require('./routes/userRoutes');
+const authRoutes = require('./routes/auth')
 require('dotenv').config();
 
 const app = express();
@@ -10,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
-
+app.use('/api/auth/', authRoutes);
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('MongoDB connected');
